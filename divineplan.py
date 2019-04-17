3985eaa02f4d94aa590fbc19f8965f8c72fcf4e1
cherenkov <cherenkov@teconpc.local>
2019-04-17 18:07:04 +0300

    working drawing

--- divineplan.py
+++ divineplan.py
@@ -7,99 +7,145 @@ import glob
 import random
 from pathlib import Path
 
+CIRCLE_BASIC_RADIUS = 40
+
+
 class StructureType(Enum):
     CLASS = 0
     METHOD = 1
     VARIABLE = 2
 
-class Graphics:
-
-    def __init__(self):
-        self._win = GraphWin("DIVINEPLAN", 800, 600)
-
-    def draw(self, objects):
-        for obj in objects:
-            obj.shape.draw(self._win)
-
-    def flush_screen(self):
-        self._win.flush()
-
 class Parser(ABC):
-
     def __init__(self, dir):
         self.tracked_files = []
         self.updated_files = []
         self.directory = dir
-
     @abstractmethod
     def parse(self, file):
         pass
-
     @abstractmethod
     def update_dir(self):
-        pass
-                
+        pass    
     def process_updated(self):
         pass
-    
     def process_tracked(self):
         pass
 
 class ParseC(Parser):
-        
+
     def parse(self, file_path, obj_container):
         with open(file_path) as file:
+            is_comment = False
             for str in file:
+                if "/*" in str and not is_comment:
+                    is_comment = True
                 if "class" in str:
-                    #TODO: creation of class elements
-                    struct = Structure(StructureType.CLASS, str.split()[str.split().index("class") + 1], prepare_pos(obj_container))
-                    obj_container.append(struct)
+                    if str.index("class") == 0 or str[str.index("class") - 1] in ["", " "]:
+                        #TODO: creation of class elements
+                        if not is_comment and not (str[1:2] == "//"):
+                            struct = Structure(StructureType.CLASS, str.split()[str.split().index("class") + 1], prepare_pos(obj_container))
+                            obj_container.append(struct)
+                if "*/" in str:
+                    is_comment = False
 
     def update_dir(self, obj_container):
         for file in glob.iglob(self.directory + '/**', recursive=True):
             if file.endswith('.h') or file.endswith('.hpp'):
                 if file not in self.tracked_files:
                     if file not in self.updated_files:
-                        print("Caught this little shit: " + file + "\n")
+                        print("Caught this: " + file + "\n")
                         self.parse(file, obj_container)
                         self.updated_files.append(file)
                         #if os.stat(file).st_mtime > os.stat(self.tracked_files[self.tracked_files.index(file)]).st_mtime:
 
 class Structure:
     def __init__(self, type, name = "Name_error", pos=Point(0,0)):
+        self.name = Text(pos, name)
         if type == StructureType.CLASS:
-            self.shape = Circle(pos, 20) #TODO: compute_radius
+            self.shape = Circle(pos, CIRCLE_BASIC_RADIUS) #TODO: compute_radius
+        if type == StructureType.METHOD:
+            self.shape = Rectangle(pos, Point(pos.getX() + 20, pos.getY() + 20))
 
 class App:
     def __init__(self):
+        self._win = GraphWin("DIVINEPLAN", 800, 600)
         self.obj = []
         self.isOn = True
-        self.graph = Graphics()
-        self.parser = ParseC(sys.argv[1])
+        if len(sys.argv) > 1:
+            self.parser = ParseC(sys.argv[1])
         self.loop()
+    
+    def draw(self):
+        for obj in self.obj:
+            obj.shape.draw(self._win)
+            obj.name.draw(self._win)
 
     def loop(self):
         """ 
             Make element drawing work
         """        
-        while self.isOn:
-            self.graph.flush_screen()
-            #self.graph._win.
-            self.parser.update_dir(self.obj)
-            self.graph.draw(self.obj)
-            self.graph._win.getMouse()
-        self.graph._win.close()
 
-def prepare_pos(obj_container):
-    x = random.choice([20, 800])
-    y = random.choice([20, 600])
+        self.parser.update_dir(self.obj)
+        self.draw()
+        self._win.getMouse()
+        #while self.isOn:
+        #    pass
+        self._win.close()
+
+
+def relocate_shape(current_pos, obj_container):
     for obj in obj_container:
         obj_x = int(obj.shape.getCenter().getX())
         obj_y = int(obj.shape.getCenter().getY())
-        if x in range(obj_x - 20, obj_x + 20): #(obj_x - 20, 0)
-            obj.shape.move(-20, 0)
-        if y in range(obj_y - 20, obj_y + 20):
-            obj.shape.move(0,-20)
-    return Point(x,y)
+        if current_pos.getX() in range(obj_x - CIRCLE_BASIC_RADIUS, obj_x) and current_pos.getY() in range(obj_y - CIRCLE_BASIC_RADIUS, obj_y):
+            current_pos.move(-CIRCLE_BASIC_RADIUS, -CIRCLE_BASIC_RADIUS)
+            relocate_shape(current_pos, obj_container)
+        elif current_pos.getX() in range(obj_x, obj_x + CIRCLE_BASIC_RADIUS) and current_pos.getY() in range(obj_y - CIRCLE_BASIC_RADIUS, obj_y):
+            current_pos.move(CIRCLE_BASIC_RADIUS, -CIRCLE_BASIC_RADIUS)
+            relocate_shape(current_pos, obj_container)
+        elif current_pos.getX() in range(obj_x - CIRCLE_BASIC_RADIUS, obj_x) and current_pos.getY() in range(obj_y, obj_y + CIRCLE_BASIC_RADIUS):
+            current_pos.move(-CIRCLE_BASIC_RADIUS, CIRCLE_BASIC_RADIUS)
+            relocate_shape(current_pos, obj_container)
+        elif current_pos.getX() in range(obj_x, obj_x + CIRCLE_BASIC_RADIUS) and current_pos.getY() in range(obj_y, obj_y + CIRCLE_BASIC_RADIUS):
+            current_pos.move(CIRCLE_BASIC_RADIUS, CIRCLE_BASIC_RADIUS)
+            relocate_shape(current_pos, obj_container)
+    return current_pos
 
-app = App()
+def prepare_pos(obj_container):
+    x = random.choice(range(100, 700))
+    y = random.choice(range(100, 500))
+    return relocate_shape(Point(x,y), obj_container)
+    # x = random.choice(range(200, 400))
+    # y = random.choice(range(200, 400))
+    # #x = 400
+    # #y = 300
+    # for obj in obj_container:
+    #     obj_x = int(obj.shape.getCenter().getX())
+    #     obj_y = int(obj.shape.getCenter().getY())
+    #     if x in range(obj_x - 40, obj_x + 40): #(obj_x - 20, 0)
+    #         x_move_distance = random.choice(range(-80, 80))
+    #         obj.shape.move(x_move_distance, 0)
+    #         obj.name.move(x_move_distance, 0)
+    #     if y in range(obj_y - 40, obj_y + 40):
+    #         y_move_distance = random.choice(range(-80, 80))
+    #         obj.shape.move(0, y_move_distance)
+    #         obj.name.move(0, y_move_distance)
+
+
+    # for i in range(100,700, 80):
+    #     for j in range(100, 500, 80):
+    #         collided = False
+    #         for obj in obj_container:
+    #             obj_x = int(obj.shape.getCenter().getX())
+    #             obj_y = int(obj.shape.getCenter().getY())
+    #             if i in range(obj_x - 40, obj_x + 40) and j in range(obj_y - 40, obj_y + 40):
+    #                 collided = True
+    #         if not collided:
+    #             return Point(i,j)
+    # return Point(400,300)
+
+def main():
+    app = App()
+
+if __name__ == '__main__':
+    main()
