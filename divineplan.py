4fe1305b95f7fa2728bce310a8f37321375bd7fb
cherenkov <cherenkov@teconpc.local>
2019-04-17 11:52:46 +0300

    first commit

--- divineplan.py
+++ divineplan.py
@@ -4,6 +4,7 @@ from enum import Enum
 import os
 import sys
 import glob
+import random
 from pathlib import Path
 
 class StructureType(Enum):
@@ -38,7 +39,6 @@ class Parser(ABC):
     def update_dir(self):
         pass
                 
-                
     def process_updated(self):
         pass
     
@@ -52,15 +52,16 @@ class ParseC(Parser):
             for str in file:
                 if "class" in str:
                     #TODO: creation of class elements
-                    struct = Structure("class","""formula""",compute_pos(obj_container))
+                    struct = Structure(StructureType.CLASS, str.split()[str.split().index("class") + 1], prepare_pos(obj_container))
                     obj_container.append(struct)
 
-    def update_dir(self):
+    def update_dir(self, obj_container):
         for file in glob.iglob(self.directory + '/**', recursive=True):
             if file.endswith('.h') or file.endswith('.hpp'):
                 if file not in self.tracked_files:
                     if file not in self.updated_files:
                         print("Caught this little shit: " + file + "\n")
+                        self.parse(file, obj_container)
                         self.updated_files.append(file)
                         #if os.stat(file).st_mtime > os.stat(self.tracked_files[self.tracked_files.index(file)]).st_mtime:
 
@@ -78,17 +79,27 @@ class App:
         self.loop()
 
     def loop(self):
+        """ 
+            Make element drawing work
+        """        
         while self.isOn:
             self.graph.flush_screen()
-            self.parser.update_dir()
+            #self.graph._win.
+            self.parser.update_dir(self.obj)
             self.graph.draw(self.obj)
+            self.graph._win.getMouse()
         self.graph._win.close()
 
-
-
-
-def compute_pos(obj_container):
-    pass
-
+def prepare_pos(obj_container):
+    x = random.choice([20, 800])
+    y = random.choice([20, 600])
+    for obj in obj_container:
+        obj_x = int(obj.shape.getCenter().getX())
+        obj_y = int(obj.shape.getCenter().getY())
+        if x in range(obj_x - 20, obj_x + 20): #(obj_x - 20, 0)
+            obj.shape.move(-20, 0)
+        if y in range(obj_y - 20, obj_y + 20):
+            obj.shape.move(0,-20)
+    return Point(x,y)
 
 app = App()
